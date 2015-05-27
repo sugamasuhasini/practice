e591948e9cba8dad91f25e50e78cd8899adcb86f
CHANDANA <chandana.ven8@yahoo.in>
2015-05-26 21:40:05 -0700

    First commit

---
+++ app/models/employee.rb
@@ -0,0 +1,3 @@
+class Employee < ActiveRecord::Base
+  attr_accessible :first_name, :last_name
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Practice::Application.config.secret_token = 'f07aae536ae44721630e1ffb8f32ac5b8b053835839904949fa6ec40b03961831f5b2ea16da552156857fdf053040938da365cd249632f1ff73451cecb58181b'
